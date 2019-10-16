661a6ce7b04fd90a7126195d87aefe0d4d01bbd0
Uladzislau Rezki (Sony) <urezki@gmail.com>
2019-10-16 11:54:37 +0200

    mm/vmalloc: respect passed gfp_mask when do preloading
    alloc_vmap_area() is given a gfp_mask for the page allocator.
    Let's respect that mask and consider it even in the case when
    doing regular CPU preloading, i.e. where a context can sleep.
    
    Signed-off-by: Uladzislau Rezki (Sony) <urezki@gmail.com>
    Acked-by: Michal Hocko <mhocko@suse.com>
    Signed-off-by: Uladzislau Rezki (Sony) <urezki@gmail.com>
    Acked-by: Michal Hocko <mhocko@suse.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Adam W. Willis <return.of.octobot@gmail.com>

--- mm/vmalloc.c
+++ mm/vmalloc.c
@@ -1112,7 +1112,7 @@ retry:
 		 * Just proceed as it is. If needed "overflow" path
 		 * will refill the cache we allocate from.
 		 */
-		pva = kmem_cache_alloc_node(vmap_area_cachep, GFP_KERNEL, node);
+		pva = kmem_cache_alloc_node(vmap_area_cachep, gfp_mask, node);
 
 	spin_lock(&vmap_area_lock);
 
