bd610d9ad6619cfdb278aebcbf390998cd2a6b81
Tyler Nijmeh <tylernij@gmail.com>
2020-02-18 17:11:06 -0800

    block: Flag to complete read requests on the same CPU
    Since the QUEUE_FLAG_SAME_FORCE flag forces every request onto its
    origin CPU, write speeds (specifically random write) suffer slightly in
    throughput since write requests do not benefit from the same cache
    locality as read requests. Allow these read requests to be routed back
    to their origin CPUs for the possibility of a cache hit, and allow write
    requests to have the possibility of local softirq completion.
    
    Signed-off-by: Tyler Nijmeh <tylernij@gmail.com>

--- include/linux/blkdev.h
+++ include/linux/blkdev.h
@@ -654,6 +654,7 @@ struct request_queue {
 #define QUEUE_FLAG_SCSI_PASSTHROUGH 27	/* queue supports SCSI commands */
 #define QUEUE_FLAG_QUIESCED    28	/* queue has been quiesced */
 #define QUEUE_FLAG_INLINECRYPT 29	/* inline encryption support */
+#define QUEUE_FLAG_SAME_READ   30	/* complete reads on same CPU */
 
 #define QUEUE_FLAG_DEFAULT	((1 << QUEUE_FLAG_NONROT) |		\
 				 (1 << QUEUE_FLAG_STACKABLE)	|	\
